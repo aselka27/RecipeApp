a8d9ee574884b502b5bf9bab0e1a0c67606bf8cb
aselka27 <asjumalieva@gmail.com>
2023-03-03 11:45:41 +0600

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by саргашкаева on 3.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
